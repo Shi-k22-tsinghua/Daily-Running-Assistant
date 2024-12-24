1fe1d196e53850aa3e6a8be9a69f5e9c164c2e65
shi-k22@mails.tsinghua.edu.cn <shi-k22@mails.tsinghua.edu.cn>
2024-12-24 21:16:12 +0800

    Fix: clear inputbox properly

--- frontend/pages/chat/chat.js
+++ frontend/pages/chat/chat.js
@@ -1,4 +1,5 @@
-// pages/AIchat/AIchat.js
+
+// pages/chat/chat.js
 const app = require('../../app.js');
 
 Page({
@@ -100,9 +101,6 @@ Page({
         });
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
     onLoad(options) {
         const nickname = wx.getStorageSync('nickname');
         if (nickname) {
@@ -112,52 +110,4 @@ Page({
         }
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
-    }
 })
